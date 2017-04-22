40ede70567e34f485abbb8e2e6c982c2acd7a446
Dan McKeon <email": null>
2017-04-22 12:49:07 -0700

    refactor calcbtn event hadnler

--- js/main.js
+++ js/main.js
@@ -31,7 +31,8 @@ document.getElementById('calcBtn').addEventListener('click', () => {
     let principal = document.getElementById("principal").value;
     let years = document.getElementById("years").value;
     let rate = document.getElementById("rate").value;
-    let {monthlyPayment, monthlyRate} = calculateMonthlyPayment(principal, years, rate);
+    let {monthlyPayment, monthlyRate, amortization} = calculateAmortization(principal, years, rate);
     document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
     document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
+    amortization.forEach(month => console.log(month));
 });
