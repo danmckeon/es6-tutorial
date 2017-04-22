b68fda5acc75d578abc21e217f2c8da5dc6d8d2a
Dan McKeon <email": null>
2017-04-22 12:57:56 -0700

    copy monthly pmt and amortization functions to mortgage.js and add export keyword to make them available as part of public api

---
+++ js/mortgage.js
@@ -0,0 +1,28 @@
+export let calculateMonthlyPayment = (principal, years, rate) => {
+    let monthlyRate = 0;
+    if (rate) {
+        monthlyRate = rate / 100 / 12;
+    }
+    let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
+    return {principal, years, rate, monthlyPayment, monthlyRate};
+};
+
+export let calculateAmortization = (principal, years, rate) => {
+    let {monthlyRate, monthlyPayment} = calculateMonthlyPayment(principal, years, rate)
+    let balance = principal;                                //initialize balance to principal
+    let amortization = [];                                  //initialize amortization to empty arr
+    for (let y = 0; y < years; y++) {
+        let interestY = 0;                                  //initialize interest pmt for year y
+        let principalY = 0;                                 //initialize principal pmt for year y
+        for (let m = 0; m < 12; m++) {
+            let interestM = balance * monthlyRate;          //interest pmt for month m
+            let principalM = monthlyPayment - interestM;    //principal pmt for month m
+            interestY = interestY + interestM;
+            principalY = principalY + principalM;
+            balance = balance - principalM;
+        }
+
+        amortization.push({principalY, interestY, balance});//pass json to amortization array for each year
+    }
+    return {monthlyPayment, monthlyRate, amortization};
+}
